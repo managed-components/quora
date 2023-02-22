2a5ea802c8ecbb9dc8d27f1cfc22fcd50f14552e
Omar Mohammad <omarmo@cloudflare.com>
2023-02-22 18:28:44 +0000

    🧹 Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { ComponentSettings, Manager, MCEvent } from '@managed-components/types';
-import { getHashedString } from './utils';
+import { ComponentSettings, Manager, MCEvent } from '@managed-components/types'
+import { getHashedString } from './utils'
 
 const sendEvent = (pixelId: string) => async (event: MCEvent) => {
-  const { client, payload } = event;
-  const em = payload.email ? await getHashedString(payload.email) : null;
+  const { client, payload } = event
+  const em = payload.email ? await getHashedString(payload.email) : null
 
   const query = {
     u: client.url.href,
     tag: payload.en || 'ViewContent',
     ts: new Date().valueOf().toString(),
     j: '1', // this is hard-coded
-    ...(em && { em })
-  };
-  
-  const params = new URLSearchParams(query).toString();
+    ...(em && { em }),
+  }
+
+  const params = new URLSearchParams(query).toString()
 
   client.fetch(`https://q.quora.com/_/ad/${pixelId}/pixel?${params}`, {
     credentials: 'include',
     keepalive: true,
-    mode: 'no-cors'
-  });
+    mode: 'no-cors',
+  })
 }
 
 export default async function (manager: Manager, settings: ComponentSettings) {
-  manager.addEventListener('pageview', sendEvent(settings.pixelId));
-  manager.addEventListener('event', sendEvent(settings.pixelId));
+  manager.addEventListener('pageview', sendEvent(settings.pixelId))
+  manager.addEventListener('event', sendEvent(settings.pixelId))
 }
